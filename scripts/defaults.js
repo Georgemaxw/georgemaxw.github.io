cc61abd47f6bd1c17dae29b4dbadd06e5a150281
Georgemaxw <39246931+Georgemaxw@users.noreply.github.com>
2021-07-13 13:04:27 -0300

    Add files via upload

--- scripts/defaults.js
+++ scripts/defaults.js
@@ -1,17 +1,16 @@
-/**/
-function func(arg){ alert(arg) }
 
 // {(function(){    })()}  
 
-//if(bbb) { console.log(1) } else { console.log(0) }
-
-
-
 var nav_open = 501 
 
+/*  
+──────────────────────────────────────────────────────
+TODO: Header-nav
+────────────────────────────────────────────────────── */
+
 var nav_menu_compact_enabled = false
-var categories_anchors = document.querySelectorAll('#nav-menu > ul > li > a')
-var divs = document.querySelectorAll('#nav-menu > ul > li > div') 
+var categories_anchors = document.querySelectorAll('.nav-menu > ul > li > a')
+var divs = document.querySelectorAll('.nav-menu > ul > li > div') 
 var index = 'none'
 var bg_nav_selector = 'url(../../images/background-nav-selector.png)  center/100% 100%'
 
@@ -19,15 +18,15 @@ document.querySelector('.nav-menu-button').onclick = function() {
 
     //if(window.innerWidth < nav_open) { 
 
-        if(document.querySelector('#nav-menu').style.display !== 'block') { 
+        if(document.querySelector('.nav-menu').style.display !== 'block') { 
 
-            document.querySelector('#nav-menu').style = 'display: block'
+            document.querySelector('.nav-menu').style = 'display: block'
             document.querySelector('.nav-menu-button :nth-child(1)').style = 'display: none'
             document.querySelector('.nav-menu-button :nth-child(2)').style = 'display: flex'
             document.querySelector('html').style = 'overflow: hidden'
             nav_menu_compact_enabled = true
         } else {
-            document.querySelector('#nav-menu').style = 'display: none'
+            document.querySelector('.nav-menu').style = 'display: none'
             document.querySelector('.nav-menu-button :nth-child(2)').style = 'display: none'
             document.querySelector('.nav-menu-button :nth-child(1)').style = 'display: block'
             document.querySelector('html').style = 'overflow: auto'
@@ -45,15 +44,15 @@ document.querySelector('.nav-menu-button').onclick = function() {
 
 window.onresize = function() { 
     if(window.innerWidth >= nav_open) {
-        document.querySelector('#nav-menu').style = 'display: block'
+        document.querySelector('.nav-menu').style = 'display: block'
     } else {
         if(nav_menu_compact_enabled === true) {
-            document.querySelector('#nav-menu').style = 'display: block'
+            document.querySelector('.nav-menu').style = 'display: block'
             document.querySelector('.nav-menu-button :nth-child(1)').style = 'display: none'
             document.querySelector('.nav-menu-button :nth-child(2)').style = 'display: flex'
             document.querySelector('html').style = 'overflow: hidden'
         } else {
-            document.querySelector('#nav-menu').style = 'display: none'
+            document.querySelector('.nav-menu').style = 'display: none'
             document.querySelector('.nav-menu-button :nth-child(2)').style = 'display: none'
             document.querySelector('.nav-menu-button :nth-child(1)').style = 'display: block'
             document.querySelector('html').style = 'overflow: auto'
@@ -74,11 +73,9 @@ window.onresize = function() {
      }*/
 }
 
-function nav_menu(arg) {
-
-    // console.log(this === categories_anchors[0])
+function nav_menu_options(arg) { 
     
-    for(var i = 0; i < categories_anchors.length; i++) {
+    for(var i = 0; i < categories_anchors.length; i++) { // <- Verifica qual <a> de categoria foi clicado.
         if(arg === categories_anchors[i]) {
 
             index = i
@@ -88,23 +85,24 @@ function nav_menu(arg) {
     
 //-------------------------------------------------------------------------------------------------
 
-    if(divs[index].style.display !== 'block') { 
+    if(divs[index].style.display !== 'block') { // <- Habilita/desabilita o <div> de subcategoria 
+                                                                           // correspondente ao <a> de categoria clicado.
 
-        for(var i = 0; i < divs.length; i++) {
+        for(var i = 0; i < divs.length; i++) { // <- Desabilita todos os <div>'s de subcategoria.
             categories_anchors[i].style = ''
             divs[i].style = 'display: none'
         }
 
-        if(window.innerWidth > nav_open) { 
+        if(window.innerWidth >= nav_open) { 
             categories_anchors[index].style = 
-            'background: '+ bg_nav_selector +'; height: 40px; border-radius: 15px; padding-left: 4px; padding-right: 4px; margin-top: 4px'
+            'background: '+ bg_nav_selector +'; height: 40px; border-radius: 15px; padding-left: 5px; padding-right: 5px; margin-top: 4px'
         }
 
         divs[index].style = 'display: block'
 
         nav_menu_compact_enabled = true
 
-        if(window.innerWidth > nav_open) { 
+        if(window.innerWidth >= nav_open) { 
             document.querySelector('html').style = 'overflow: hidden'
         }
 
@@ -116,18 +114,15 @@ function nav_menu(arg) {
 
         nav_menu_compact_enabled = false
 
-        if(window.innerWidth > nav_open) { 
+        if(window.innerWidth >= nav_open) { 
             document.querySelector('html').style = 'overflow: auto'
         }
     }
-} 
+}
 
+// Header-nav button:
 
-
-
-
-
-document.querySelector('#nav-menu > ul > li:nth-last-of-type(1)').onclick = function() {
+document.querySelector('.nav-menu > ul > li:nth-last-of-type(1)').onclick = function() { // <- Hide Header-nav
 
     for(var i = 0; i < divs.length; i++) {
         categories_anchors[i].style = ''
@@ -143,11 +138,11 @@ document.querySelector('#nav-menu > ul > li:nth-last-of-type(1)').onclick = func
     
     document.querySelector('header').style.display = 'none'
     document.querySelector('.block-hack').style.display = 'none'
-    document.querySelector('.show-header-button').style.display = 'block'
+    document.querySelector('.headerNav-button').style.display = 'block'
 }
 
-document.querySelector('.show-header-button').onclick = function() {
+document.querySelector('.headerNav-button').onclick = function() { // <- Show Header-nav
     document.querySelector('header').style.display = 'block'
     document.querySelector('.block-hack').style.display = 'block'
-    document.querySelector('.show-header-button').style.display = 'none'
+    document.querySelector('.headerNav-button').style.display = 'none'
 }
